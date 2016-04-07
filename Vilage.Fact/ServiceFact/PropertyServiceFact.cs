9fd1543a49b7d11f6db340b5545e355ca0d775d4
Watuka Thuannimit <watuka.t@gmail.com>
2016-04-07 18:28:18 +0700

    Add servicefact Property and IHouseFakeDb

---
+++ Vilage.Fact/ServiceFact/PropertyServiceFact.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Should;
+using Village.Model;
+using Moq;
+using Village.Service.Repository;
+using Village.Service;
+
+namespace Vilage.Fact.ServiceFact
+{
+    public class PropertyServiceFact
+    {
+        public class HouseFact
+        {
+            private House h;
+            public HouseFact()
+            {
+                h = new House();
+            }
+
+            [Fact]
+            public void GetHouse_MustCall_HouseMockRepo()
+            {
+                Mock<IHouseFakeDb> fakeRepo = new Mock<IHouseFakeDb>();
+                fakeRepo.Setup(fake => fake.GetAllHouse());
+                PropertyService sut = new PropertyService(fakeRepo.Object);
+                List<House> house = sut.GetAllHouse();
+                fakeRepo.Verify(fake => fake.GetAllHouse(), Times.Exactly(1));
+            }
+
+        }
+    }
+}
