ae09c925de2c472a9ce55f441c8ec3a7b92f9126
Prasert.k <Prasert.k@CNX-NBTON>
2016-04-08 09:40:02 +0700

    Update test.bat

--- test.bat
+++ test.bat
@@ -4,6 +4,6 @@ Packages\xunit.runner.console.2.1.0\tools\xunit.console ^
 Vilage.Fact\bin\Debug\Vilage.Fact.dll ^
 	-parallel all ^
 	-html Result.html ^
-	-nologo
-	
+	-nologo ^
+    -verbose
 @echo on 
